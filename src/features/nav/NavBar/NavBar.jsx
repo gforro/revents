d47d3d173278fffc51429dce0ea295f45895514c
gabriel.forro <gabriel.forro@cloudfarms.com>
2019-06-10 13:48:52 +0200

    End of section 6

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,27 +1,39 @@
 import React, {Component} from 'react';
 import {Button, Container, Menu} from 'semantic-ui-react';
+import {Link, NavLink} from 'react-router-dom';
+import SignedInMenu from '../Menus/SignedInMenu';
+import SignedOutMenu from '../Menus/SignedOutMenu';
+import {withRouter} from 'react-router-dom';
 
 class NavBar extends Component {
+  state = { authenticated: false}
+
+  handleSignIn = () => this.setState({authenticated: true});
+
+  handleSignOut = () => {
+    this.setState({authenticated: false});
+    this.props.history.push('/');
+  }
+
   render() {
+    const {authenticated} = this.state;
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header>
-            <img src="assets/logo.png" alt="logo" />
+          <Menu.Item as={NavLink} exact to='/' header>
+              <img src="/assets/logo.png" alt="logo" />
             Re-vents
           </Menu.Item>
-          <Menu.Item name="Events" />
-          <Menu.Item>
+          <Menu.Item as={NavLink} to='/events' name="Events" />
+          <Menu.Item as={NavLink} to='/people' name="People" />
+          <Menu.Item as={Link} to='/createEvent'>
             <Button floated="right" positive inverted content="Create Event" />
           </Menu.Item>
-          <Menu.Item position="right">
-            <Button basic inverted content="Login" />
-            <Button basic inverted content="Sign Out" style={{marginLeft: '0.5em'}} />
-          </Menu.Item>
+          {authenticated ? <SignedInMenu signOut={this.handleSignOut}/> : <SignedOutMenu signIn={this.handleSignIn}/> }
         </Container>
       </Menu>
     );
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
